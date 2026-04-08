1b11c5f92dbc2fd7181566cc1b8e33fddaf01628
RiJaDivi <risadivis@gmail.com>
2026-04-08 18:31:06 +0200

    Add initial map structure and print functionality
    Initialize a 2D array and set up basic structure for map handling.

--- hewow.cs
+++ hewow.cs
@@ -1,1 +1,31 @@
+string[][] map = new map[maple][maple];
 Console.Writeline("hewow");
+maple = 8
+ int x = 0;
+  int y = 0;
+
+
+
+for (int i = 0; i < maple; i++){
+  for (int j = 0; j < maple; j++){
+map[i][j] = .
+  }
+}
+
+static string Plaher(int x,int y) {
+map[x][y] = P
+ }
+
+static string Maprint(string map[][]) {
+    for (int i = 0; i < maple; i++)
+    {
+        for (int j = 0; j < maple; j++)
+        {
+            Console.Write(pole[i][j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
+
+
+
