b300d890684dafe6f4c007e7ae833f850a4048d0
RiJaDivi <risadivis@gmail.com>
2026-04-17 17:38:10 +0200

    Refactor map and základy player movement
    Refactor the map initialization and player movement logic. Update method names and improve code structure.

--- hewow.cs
+++ hewow.cs
@@ -1,31 +1,78 @@
-string[][] map = new map[maple][maple];
-Console.Writeline("hewow");
-maple = 8
- int x = 0;
-  int y = 0;
+using System;
 
+class Program
+{
+    static int maple = 8;
+    static string[][] map;
 
-
-for (int i = 0; i < maple; i++){
-  for (int j = 0; j < maple; j++){
-map[i][j] = .
-  }
-}
-
-static string Plaher(int x,int y) {
-map[x][y] = P
- }
-
-static string Maprint(string map[][]) {
-    for (int i = 0; i < maple; i++)
+    static void Main()
     {
-        for (int j = 0; j < maple; j++)
+        map = new string[maple][];
+
+        for (int i = 0; i < maple; i++)
         {
-            Console.Write(pole[i][j] + " ");
+            map[i] = new string[maple];
+            for (int j = 0; j < maple; j++)
+            {
+                map[i][j] = ".";
+            }
         }
-        Console.WriteLine();
+	bool esc = false;
+        int x = 6;
+        int y = 0;
+	while (true)
+	{
+if (esc)
+{
+	break;
+}
+var key = Console.ReadKey(false);
+switch (key.Key)
+{
+	
+    case ConsoleKey.Escape:
+	Console.WriteLine("Končím");
+	esc = true;
+        break;
+
+    case ConsoleKey.W:
+    case ConsoleKey.UpArrow:
+        Console.WriteLine("Nahoru");
+        break;
+    case ConsoleKey.S:
+    case ConsoleKey.DownArrow:
+        Console.WriteLine("Dolu");
+        break;
+    case ConsoleKey.A:
+    case ConsoleKey.LeftArrow:
+        Console.WriteLine("Leva");
+        break;
+    case ConsoleKey.D:
+    case ConsoleKey.RightArrow:
+	y ++;
+        break;
+}
+	}
+        Player(x, y);
+        MapPrint();
+
+        Console.ReadKey(true);
+    }
+
+    static void Player(int x, int y)
+    {
+        map[x][y] = "P";
+    }
+
+    static void MapPrint()
+    {
+        for (int i = 0; i < maple; i++)
+        {
+            for (int j = 0; j < maple; j++)
+            {
+                Console.Write(map[i][j] + " ");
+            }
+            Console.WriteLine();
+        }
     }
 }
-    var key = Console.ReadKey(true);
-
-
